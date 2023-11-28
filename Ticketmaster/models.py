e38bba100137558edd01ba19cfb98a082486086f
fyasmin02 <ocookie750@gmail.com>
2023-11-27 20:43:03 -0500

    Trying to store data

--- Ticketmaster/models.py
+++ Ticketmaster/models.py
@@ -11,14 +11,16 @@ class Userprofile(models.Model):
 
 
 class Event(models.Model):
+    eventid = models.CharField('Event Name', max_length=200)
     name = models.CharField('Event Name', max_length=200)
     venue = models.CharField('Venue Name', max_length=200)
     address = models.CharField('Venue Address', max_length=150)
     city = models.CharField('Event City', max_length=150)
     state = models.CharField('Event State', max_length=100)
-    start_date = models.DateTimeField('Event DateTime')
-    ticket_link = models.URLField('Ticket Link')
-    image_url = models.URLField('Image URL')
+    start_date = models.CharField('Event Date',max_length=100)
+    start_time = models.CharField('Event Time', max_length=100)
+    ticket_link = models.URLField('Ticket Link', max_length=200)
+    image_url = models.URLField('Image URL', max_length=200)
 
     def __str__(self):
         return self.name
